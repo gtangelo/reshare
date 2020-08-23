06d949a2e9f18f0c0d40d36959dbb0790cea46ff
gtangelo <65492346+gtangelo@users.noreply.github.com>
2020-08-23 22:56:28 +1000

    started working on post framework

--- app.py
+++ app.py
@@ -1,44 +1,68 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
-feed_posts = [
-    {
-        'id' : 0,
-        'title': 'I am a comp god',
-        'content': 'comp god rant',
-        'author': 'Ray Zone',
-        'date': '01/06/20',
-        'likes': 53,
-        'dislikes': 1
-    },
-    {
-        'id' : 1,
-        'title': 'I am a sci god',
-        'content': 'comp 2041 rant',
-        'author': 'Alex Zone',
-        'date': '01/06/20',
-        'likes': 53,
-        'dislikes': 1
-    },
-      {
-        'id' : 2,
-        'title': 'I am a math god',
-        'content': 'king rant',
-        'author': 'John Smith',
-        'date': '01/06/20',
-        'likes': 53,
-        'dislikes': 1
-    }
-]
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(app)
+
+# Model for database contating posts
+class FeedPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(20), nullable=False, default='Unknown')
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "Post ID: " + str(self.id)
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/home')
+@app.route('/home', methods=['GET', 'POST'])
 def home():
-    return render_template("home.html", feed=feed_posts)
+    # Get all data from the form, process it and send it to the db
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_content = request.form['content']
+        post_author = request.form['author']
+        new_post = FeedPost(title = post_title, content = post_content, author = post_author)
+        db.session.add(new_post)
+        # Save posts for future
+        db.session.commit()
+        return redirect('/home')
+    else:
+        # Get post from db and order it by date in descending order
+        feed_posts = FeedPost.query.order_by(FeedPost.date.desc()).all()
+        return render_template("home.html", feed=feed_posts)
+
+@app.route('/home/delete/<int:id>')
+def delete_post(id):
+    post = FeedPost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/home')
+
+@app.route('/home/edit/<int:id>', methods=['GET', 'POST'])
+def edit_post(id):
+    post = FeedPost.query.get_or_404(id)
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.content = request.form['content']
+        post.author = request.form['author']
+        db.session.commit()
+        return redirect('/home')
+    else:
+        return render_template('edit.html', post=post)
+
+def settings():
+    return render_template("settings.html")
+
 
 @app.route('/settings')
 def settings():
